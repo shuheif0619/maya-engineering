954e64a83a5b24647ec82ce62a04626cc1979e1b
Shuhei Fujita <130586956+shuheif0619@users.noreply.github.com>
2025-10-27 23:42:46 +0900

    Create recruit-circles.php

---
+++ components/recruit-circles/recruit-circles.php
@@ -0,0 +1,36 @@
+<!-- ========== Recruit Circles: START ========== -->
+<div class="recruit__circles">
+  <!-- 8の字状の円の画像 -->
+  <div class="recruit__bg-circle" aria-hidden="true"></div>
+
+  <div class="recruit__circle-flex">
+    <div class="recruit__job-type recruit__job-type--left js-fadeIn">
+      <figure class="recruit__job-image">
+        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/mayaengineering.svg"
+             alt="MAYAエンジニアリングのアイコン" width="246" height="140" loading="lazy" decoding="async">
+      </figure>
+      <div class="recruit__job-labels">
+        <span class="recruit__job-label">受託</span>
+        <span class="recruit__job-label">自社在籍</span>
+      </div>
+      <p class="recruit__job-description">
+        高度なIT技術と豊富な実務経験を活かした、自社在籍の受託部門です。大手通信キャリアで培った知見を基に、安定性と専門性を両立しながら、
+        キャリアアップを目指せる環境です。
+      </p>
+    </div>
+
+    <div class="recruit__job-type recruit__job-type--right js-fadeIn">
+      <figure class="recruit__job-image">
+        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/mayastaffing.svg"
+             alt="MAYAスタッフィングのアイコン" width="246" height="140" loading="lazy" decoding="async">
+      </figure>
+      <div class="recruit__job-labels">
+        <span class="recruit__job-label">人材派遣</span>
+      </div>
+      <p class="recruit__job-description">
+        大手通信キャリアを中心としたSES事業部門です。営業から構築、運用・保守まで一貫して担当できる、実力派のIT人材が活躍中。希望に沿った案件で、理想のキャリアを実現できます。
+      </p>
+    </div>
+  </div>
+</div>
+<!-- ========== Recruit Circles: END ========== -->
